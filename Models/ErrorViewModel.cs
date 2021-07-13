e9e4f0973653d2a0dc4c6fcec3a0aedd8fa9c0e9
Kamal <email2kml@gmail.com>
2021-07-13 02:18:22 +0000

    d

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace dotnetx1x1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
